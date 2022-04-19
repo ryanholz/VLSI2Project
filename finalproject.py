833f09a8e04227d3ac6ef7b19b6303409053fe82
Kevin <ferreira.k@ufl.edu>
2022-04-19 16:38:16 -0400

    small changes

--- finalproject.py
+++ finalproject.py
@@ -4,24 +4,31 @@ import threading
 
 # Function that outputs a string
 def print_str(fileName, threadNum):
-	# Open a file and write to it
-	wrapper = open(fileName, "wb")
+	# Open verilog file to read and create a new file and write to it
+	wrapper = open('wrapper_{0}'.format(fileName), "wb")
+	socFile = open(fileName, "rb")
 
 	# Print to file then output closed thread notification
 	wrapper.write('{0}: {1}\n'.format(threadNum, fileName))
 	print 'End of thread ', threadNum, '\n'
 	
-	# Close file after finished
+	# Close files after finished
 	wrapper.close()
+	socFile.close()
 
 # Read command line inputs
 t = []
 
+# Creating threads 
 for i in range(1, len(sys.argv)):
 	t.append(threading.Thread(target=print_str, args=(str(sys.argv[i]), i)))
 
+# Start thread execution after thread creation
 for i in range(0, len(sys.argv)):
 	t[i].start();
 
+# Wait for each thread to finish executing
 for i in range(1, len(sys.argv)):
 	t[i].join();
+
+print 'Wrapper Generation complete!\n'
