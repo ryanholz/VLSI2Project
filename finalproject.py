6bcd26544f32869a83d94e1c3712c2d0f2b2658e
Kevin <ferreira.k@ufl.edu>
2022-04-22 15:48:33 -0400

    additional wrapper set up. additional test text added

--- finalproject.py
+++ finalproject.py
@@ -12,25 +12,75 @@
 import sys
 import threading
 # TODO: Uncomment once file is working
-#import InputOutputFinger
+import iverilogReader as ivr
 
 # Function that outputs a string
 def print_str(fileName, threadNum):
 	# Open verilog file to read and create a new file and write to it
 	wrapper = open('wrapper_{0}'.format(fileName), "wb")
-	#socFile = open(fileName, "rb")
-
-	# TODO:
-	# Continue to implement once parser is completed
-
-	# Print to file then print end of thread notification
-	wrapper.write('/*\n* Wrapper code generated using finalproject.py\n*/\n')
-	wrapper.write('{0}: {1}\n'.format(threadNum, fileName))
-	print 'End of thread ', threadNum, '\n'
 	
+	# Read Module name and store in variable
+	ivOutFile = open(fileName, "rb")
+	moduleName = ivr.findModule(ivOutFile)
+
+	# Read input ports and store in variable
+	ivOutFile = open(fileName, "rb")
+	inputList = ivr. findInputs(ivOutFile)
+
+	# Read output ports and store in variable
+	ivOutFile = open(fileName, "rb")
+	outputList = ivr.findOutputs(ivOutFile)
+	
+	# Generate comments at the beginning of file
+	wrapper.write('/*\n* Wrapper code generated using finalproject.py\n')
+	wrapper.write('* Module name: {0}\n'.format(moduleName))
+	wrapper.write('* Inputs: {0}\n'.format(inputList))
+	wrapper.write('* Outputs: {0}\n*\n'.format(outputList))
+	wrapper.write('* Generate by thread {0}. From file {1}\n'.format(threadNum, fileName))
+	wrapper.write('*/\n')
+
+	# Generate module definition
+	wrapper.write('module {0} ('.format(moduleName))
+	for i in range(0, len(inputList)):
+		if '[' in inputList[i]:
+			# Skip if data range is present
+			pass
+		else:
+			wrapper.write('{0}, '.format(inputList[i]))
+	for i in range(0, len(outputList)):
+		if '[' in outputList[i]:
+			# Skip input range in module declaration
+			pass
+		else:
+			wrapper.write('{0}'.format(outputList[i]))
+			if i < len(outputList) - 1:
+				wrapper.write(', ')
+	wrapper.write(')\n\n')
+
+	# Declare input signals
+	wrapper.write('/*\n* Input Signals\n*/\n')
+	i = 0
+	while(i < len(inputList)):
+		if '[' in inputList[i]:
+			wrapper.write('input {0}{1};\n'.format(inputList[i], inputList [i+1]))
+			i += 1
+		else:
+			wrapper.write('input {0};\n'.format(inputList[i]))
+		i += 1
+	
+	# Declare output signals
+	wrapper.write('\n/*\n*Output Signals\n*/\n')
+	i = 0
+	while(i < len(outputList)):
+		if '[' in outputList[i]:
+			wrapper.write('output {0}{1}\n'.format(outputList[i], outputList[i+1]))
+			i += 1
+		else:
+			wrapper.write('output {0}\n'.format(outputList[i]))
+		i += 1
 	# Close files after finished
 	wrapper.close()
-	#socFile.close()
+	ivOutFile.close()
 
 # Creating array to hold all the thread data
 t = []
@@ -40,11 +90,11 @@ for i in range(1, len(sys.argv)):
 	t.append(threading.Thread(target=print_str, args=(str(sys.argv[i]), i)))
 
 # Start thread execution after thread creation
-for i in range(0, len(sys.argv)):
+for i in range(0, len(sys.argv) - 1):
 	t[i].start();
 
 # Wait for each thread to finish executing
-for i in range(1, len(sys.argv)):
+for i in range(1, len(sys.argv) - 1):
 	t[i].join();
 
 print 'Wrapper Generation complete!\n'
--- iverilogReader.py
+++ iverilogReader.py
@@ -22,7 +22,8 @@ def findInputs(fileName):
             if 'Width:' in contents[index + 1]: # Check if input contains multiple bits
                 upperBit = nextWord('IntConst:', contents[index+2].split()) # Find upper bit
                 lowerBit = nextWord('IntConst:', contents[index+3].split()) # Find lower bit
-                inputList.append('[' + upperBit + ':' + lowerBit + '] ' + inputName[:-1]) # Add info to array
+                inputList.append('[' + upperBit + ':' + lowerBit + '] ')# + inputName[:-1]) # Add info to array
+		inputList.append(inputName)
             else:
                 inputList.append(inputName[:-1]) # Add info to array
         index += 1
@@ -39,26 +40,27 @@ def findOutputs(fileName):
             if 'Width:' in contents[index + 1]: # Check if input contains multiple bits
                 upperBit = nextWord('IntConst:', contents[index+2].split()) # Find upper bit
                 lowerBit = nextWord('IntConst:', contents[index+3].split()) # Find lower bit
-                outputList.append('[' + upperBit + ':' + lowerBit + '] ' + outputName[:-1]) # Add info to array
+                outputList.append('[' + upperBit + ':' + lowerBit + '] ')# + outputName[:-1]) # Add info to array
+		outputList.append(outputName[:-1])
             else:
                 outputList.append(outputName[:-1]) # Add info to array
         index += 1
     return outputList
 
 #Find module name and inputs and outputs of given file
-file1Name = "testText.txt"
-file1 = open(file1Name, "r")
-inputList = findInputs(file1)
-file1 = open(file1Name, "r")
-outputList = findOutputs(file1)
-file1 = open(file1Name, "r")
-moduleName = findModule(file1)
+#file1Name = "testText.txt"
+#file1 = open(file1Name, "r")
+#inputList = findInputs(file1)
+#file1 = open(file1Name, "r")
+#outputList = findOutputs(file1)
+#file1 = open(file1Name, "r")
+#moduleName = findModule(file1)
 
 # Print results
-print('Module:', moduleName)
-print('Inputs:', inputList)
-print('Outputs:', outputList)
+#print('Module:', moduleName)
+#print('Inputs:', inputList)
+#print('Outputs:', outputList)
 
 # Closing text file
-file1.close()
-print('Done')
+#file1.close()
+#print('Done')
