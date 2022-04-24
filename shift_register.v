69392f67977baa34c807bb4c60537ff3622a7b0c
kevinf03 <ferreira.k@ufl.edu>
2022-04-24 03:32:17 -0400

    moarrrrr commentssssss

--- shift_register.v
+++ shift_register.v
@@ -2,15 +2,30 @@
  * Shift Register
  * Simple shift register design that takes a 32-bit input
  * and shifts it to a single output
+ *
+ * Shift register port description:
+ * clk, rst
+ * 	Clock and reset signals. Self explanatory.
+ * load
+ * 	Signal used to load the shift register with new data.
+ * socOutput
+ * 	Output of the SoC that gets shifted out through the jtagOutput
+ * 	signal.
+ * jtagOutput
+ * 	Output of the shift register that will get shifted out through
+ * 	the TDO JTAG output pin.
  */
 
 module shift_register #(parameter LENGTH = 32) (clk, rst, socOutput, load, jtagOutput);
 
+	/* Inputs */
 	input clk, rst, load;
 	input [LENGTH-1:0] socOutput;
 
+	/* Outputs */
 	output jtagOutput;
 
+/* Additional signal used for shifting */
 reg [LENGTH-1:0] socData;
 
 /* Always block to trigger at the positive edge of the clock */
@@ -23,13 +38,13 @@ begin
 	/* Load signal for when to load new data into shift register */
 	else if(load == 1)
 		socData <= socOutput;
-	/* If no reset or load then shift conents */
+	/* If no reset or load then shift contents of register*/
 	else
 		socData <= socData >> 1;
 
 end
 
-/* Concurrent statement assigning output */
+/* Concurrent statement assigning jtagOutput */
 assign jtagOutput = socData[0];
 
 endmodule
