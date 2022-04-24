031ae097413b209eb6729020a1c29fd66a5e5bcc
ryanholz <49693249+ryanholz@users.noreply.github.com>
2022-04-24 14:26:21 -0400

    Add comments

--- pyverilogReader.py
+++ pyverilogReader.py
@@ -4,15 +4,19 @@ from pyverilog.vparser.parser import VerilogParser
 from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
 
 def runPyVerilog(fileName):
-    parser = VerilogParser()
-    expected_ast = parser.parse(open(fileName).read())
-    codegen = ASTCodeGenerator()
-    expected_code = codegen.visit(expected_ast)
+    parser = VerilogParser() # Initialize parser
+    expected_ast = parser.parse(open(fileName).read()) # Parse verilog file
+    codegen = ASTCodeGenerator() # Initialize code generator
+    expected_code = codegen.visit(expected_ast) # Generate text from parsed file
+    
+    # Reformat text to be more readable
     new = expected_code.replace(';', '; \n')
     new = new.replace(',', ', \n')
     new = new.replace('  ', '')
     new = new.replace('\n\n\n', '\n')
     new = new.replace('\n\n', '\n')
+    
+    # Transform string into array of of strings
     result = []
     word = ''
     for c in new:
@@ -41,7 +45,7 @@ def findModule(code) :
         index += 1
     index = 0
     for c in moduleName:
-        if c == '\n' or c == '(':
+        if c == '\n' or c == '(': # Ignore characters after module name
             moduleName = moduleName[:index]
             break
         index += 1            
@@ -52,7 +56,7 @@ def findInputs(code):
     index = 0
     for word in code: # For every line in the file
         if word == '\ninput': # If the word 'input' appears in the line
-            if code[index + 1][0] == '[':
+            if code[index + 1][0] == '[': # Search for multibit port
                 inputList.append(code[index + 1] + ' ')
                 inputList.append(code[index + 2][:-1])
             else:
@@ -65,24 +69,16 @@ def findOutputs(code):
     index = 0
     for word in code: # For every line in the file
         if word == '\noutput': # If the word 'input' appears in the line
-            if code[index + 1] == 'reg':
-                if code[index + 2][0] == '[':
+            if code[index + 1] == 'reg': # Ignore 'reg' after 'output'
+                if code[index + 2][0] == '[': # Search for multibit port
                     outputList.append(code[index + 2] + ' ')
                     outputList.append(code[index + 3][:-1])
                 else:
                     outputList.append(code[index + 2][:-1])
-            elif code[index + 1][0] == '[':
+            elif code[index + 1][0] == '[': # Search for multibit port
                 outputList.append(code[index + 1] + ' ')
                 outputList.append(code[index + 2][:-1])
             else:
                 outputList.append(code[index + 1][:-1])
         index += 1
     return outputList
-
-# pvOutput = runPyVerilog("uart_top.v")
-# pvOutput = reformat(pvOutput)
-# print(pvOutput)
-# print("Module: {}".format(findModule(pvOutput)))
-# print("Inputs: {}".format(findInputs(pvOutput)))
-# print("Outputs: {}".format(findOutputs(pvOutput)))
-# print("Done")
