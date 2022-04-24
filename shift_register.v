fa9b41a53a628bd8bf67d4904f49a08848a675c3
Kevin <ferreira.k@ufl.edu>
2022-04-24 01:46:49 -0400

    shift register code pushed. needs testing

---
+++ shift_register.v
@@ -0,0 +1,35 @@
+/*
+ * Shift Register
+ * Simple shift register design that takes a 32-bit input
+ * and shifts it to a single output
+ */
+
+module shift_register #(parameter LENGTH = 32) (clk, rst, socOutput, load, jtagOutput)
+
+	input clk, rst, load;
+	input [LENGTH-1:0] socOutput;
+
+	output jtagOutput;
+
+reg [LENGTH-1:0] socData;
+
+/* Always block to trigger at the positive edge of the clock */
+always @(posedge clk)
+begin
+
+	/* Synchronous reset */
+	if(rst == 1)
+		socData <= 0;
+	/* Load signal for when to load new data into shift register */
+	else if(load == 1)
+		socData <= socOutput;
+	/* If no reset or load then shift conents */
+	else
+		socData <= socData << 1;
+
+end
+
+/* Concurrent statement assigning output */
+jtagOutput <= socData[0];
+
+endmodule
---
+++ shift_register_tb.v
@@ -0,0 +1,55 @@
+/*
+ * Testbench for shift register module
+ * Testing correct functionality in shift register
+ */
+
+module shift_register_tb();
+
+	/* Parameter definition for shift register length */
+	localparam SHFT_LENGTH = 32;
+	
+	/* Input and output signals declared as regs or wires */
+	reg clk, rst, load;
+	reg [LENGTH-1:0] socOutput;
+
+	wire jtagOutput;
+
+/* Create instance of the shift register module to test */
+shift_register #(.LENGTH(SHFT_LENGTH)) SHFT_REG_UUT (.clk(clk), .rst(rst), .socOutput(socOutput), .jtagOutput(jtagOutput))
+
+/* Begin testing */
+intial
+begin
+	
+	/* Define initial clock state
+	clk <= 0;
+	
+	/* Test for reset conditions */
+	rst <= 1;
+	load <= 1;
+	socOutput <= 132;
+	#10;
+
+	/* Check for data load */
+	rst <= 0;
+	load <= 1;
+	socOutput <= 83;
+	#10;
+
+	/* Test for shifting */
+	rst <= 0;
+	load <= 0;
+	socOutput <= 165;
+	#100;
+
+end
+
+/* Always block for clock generation */
+always
+begin
+
+	#5 clk <= ~clk;
+
+end
+
+endmodule
